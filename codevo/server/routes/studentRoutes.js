9e5b6145c895e0967b9d6fe6182dd557bee8a9f5
srd-33 <skanda.learn@gmail.com>
2026-08-20 12:09:23 +0530

    Fix duplicate loginStudent import

--- codevo/server/routes/studentRoutes.js
+++ codevo/server/routes/studentRoutes.js
@@ -1,26 +1,28 @@
 import express from "express";
+
 import {
   registerStudent,
+  loginStudent,
+  googleLogin,
+  googleStudentLogin,
   getStudents,
+  getStudentById,
+  updateStudentStatus,
   deleteStudent,
-   updateStudentStatus,
-   loginStudent,
-     getStudentById,googleLogin,
-       googleStudentLogin,  loginStudent,
 } from "../controllers/studentController.js";
 
 const router = express.Router();
 
+// Authentication
 router.post("/register", registerStudent);
 router.post("/login", loginStudent);
 router.post("/google", googleLogin);
 router.post("/google/login", googleStudentLogin);
 
+// Student Management
 router.get("/", getStudents);
 router.get("/:id", getStudentById);
-
-router.delete("/:id", deleteStudent);
 router.put("/:id/status", updateStudentStatus);
-
+router.delete("/:id", deleteStudent);
 
 export default router;
